dc223dbaf6dd607f89a47a52e8c7db09b696eec4
rinat-lucky <rinat1kam@mail.ru>
2021-08-24 03:44:00 +0500

    23

--- src/js/script.js
+++ src/js/script.js
@@ -1,80 +1,75 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const tabs = document.querySelectorAll('.tabheader__item');
-    const tabsContent = document.querySelectorAll('.tabcontent');  
-    const tabsParent = document.querySelector('.tabheader__items');
+    // const tabs = document.querySelectorAll('.tabheader__item');
+    // const tabsContent = document.querySelectorAll('.tabcontent');  
+    // const tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-            // item.classList.add('hide');
-            // item.classList.remove('show', 'fade');
-        });
+    // function hideTabContent() {
+    //     tabsContent.forEach(item => {
+    //         item.style.display = 'none';
+    //     });
 
-        tabs.forEach(tab => {
-            tab.classList.remove('tabheader__item_active');
-        });
-    }
+    //     tabs.forEach(tab => {
+    //         tab.classList.remove('tabheader__item_active');
+    //     });
+    // }
 
-    function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        // tabsContent[i].classList.add('show', 'fade');
-        // tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
+    // function showTabContent(i = 0) {
+    //     tabsContent[i].style.display = 'block';
+    //     tabs[i].classList.add('tabheader__item_active');
+    // }
 
-    hideTabContent();
-    showTabContent();
+    // hideTabContent();
+    // showTabContent();
 
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
+    // tabsParent.addEventListener('click', (event) => {
+    //     const target = event.target;
 
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
+    //     if (target && target.classList.contains('tabheader__item')) {
+    //         tabs.forEach((item, i) => {
+    //             if (target == item) {
+    //                 hideTabContent();
+    //                 showTabContent(i);
+    //             }
+    //         });
+    //     }
+    // });
+
+
+    new Swiper('.interior__slider-container', {
+        navigation: {
+            nextEl: '.swiper-button-next1',
+            prevEl: '.swiper-button-prev1'
+        },
+        grabCursor: true,
+        loop: true,
+
+        breakpoints: {
+            375: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 2
+            }
         }
     });
 
+    new Swiper('.news__slider-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        grabCursor: true,
+        loop: true,
+        slidesPerView: 3,
 
-    // new Swiper('.interior__slider-container', {
-    //     navigation: {
-    //         nextEl: '.swiper-button-next1',
-    //         prevEl: '.swiper-button-prev1'
-    //     },
-    //     grabCursor: true,
-    //     slidesPerGroup: 1,
-    //     loop: true,
-
-    //     breakpoints: {
-    //         375: {
-    //             slidesPerView: 1
-    //         },
-    //         768: {
-    //             slidesPerView: 2
-    //         }
-    //     }
-    // });
-
-    // new Swiper('.news__slider-container', {
-    //     navigation: {
-    //         nextEl: '.swiper-button-next',
-    //         prevEl: '.swiper-button-prev'
-    //     },
-    //     grabCursor: true,
-    //     slidesPerGroup: 1,
-    //     loop: true,
-
-    //     breakpoints: {
-    //         375: {
-    //             slidesPerView: 1
-    //         },
-    //         768: {
-    //             slidesPerView: 2
-    //         }
-    //     }
-    // });
+        breakpoints: {
+            375: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 2
+            }
+        }
+    });
 });
